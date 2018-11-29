af3ebdff54b452834dc9fe4026914ca9b2ab40da
olegush <43958767+olegush@users.noreply.github.com>
2018-11-29 11:10:29 +0300

    improved the code readability

--- site_gen.py
+++ site_gen.py
@@ -2,7 +2,6 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
 import os
 from markdown2 import Markdown
-import string
 import re
 import time
 
@@ -38,44 +37,57 @@ def write_index_file(template, topics, articles):
         file.write(index_str)
 
 
+def get_md_data(dir, path):
+    with open(
+        os.path.join(dir, path),
+        encoding='utf-8'
+    ) as file:
+        return file.read()
+
+
+def write_article_file(path, filename, filext, article_str):
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    with open(
+        os.path.join(path, filename + filext),
+        mode='w',
+        encoding='utf-8'
+    ) as file:
+        file.write(article_str)
+
+
 def write_articles_files(template, topics, articles):
     template_article = get_env().get_template(template)
     markdowner = Markdown()
     for topic in topics:
         for article in articles:
             if article['topic'] in topic['slug']:
-                dir, fullfilename = os.path.split(article['path'])
-                filename, filext = os.path.splitext(fullfilename)
+                dir, full_filename = os.path.split(article['path'])
+                filename, filext = os.path.splitext(full_filename)
                 path_to_articles_dir = os.path.join('docs/articles', dir)
-                with open(
-                    os.path.join('articles', article['source']),
-                    encoding='utf-8'
-                ) as file:
-                    article_md = file.read()
+                article_md = get_md_data('articles', article['source'])
                 article_str = template_article.render(
                     url=article['path'],
                     topic_title=topic['title'],
                     article_title=article['title'],
                     article_content=markdowner.convert(article_md)
                 )
-                if not os.path.isdir(path_to_articles_dir):
-                    os.mkdir(path_to_articles_dir)
-                with open(
-                    os.path.join(path_to_articles_dir, filename + filext),
-                    mode='w',
-                    encoding='utf-8'
-                ) as file:
-                    file.write(article_str)
+                write_article_file(
+                    path_to_articles_dir,
+                    filename,
+                    filext,
+                    article_str
+                )
 
 
 if __name__ == '__main__':
-    startTime = time.time()
-    data = load_data('config.json')
-    topics = sorted(data['topics'], key=lambda x: x['title'])
-    articles = get_new_articles_list(data['articles'])
+    start_time = time.time()
+    data_for_site = load_data('config.json')
+    topics = sorted(data_for_site['topics'], key=lambda x: x['title'])
+    articles = get_new_articles_list(data_for_site['articles'])
     print('Data loaded and formatted')
     write_index_file('index.html', topics, articles)
     print('Index.html created')
     write_articles_files('article.html', topics, articles)
     print('Articles created')
-    print('{:.2f} seconds'.format(time.time() - startTime))
+    print('{:.2f} seconds'.format(time.time() - start_time))
